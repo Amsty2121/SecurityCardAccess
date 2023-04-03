f47493ede05c283f88754fc7ab9fadac7c6adce3
ChepteaCatalin <cheptea.catalin@mail.ru>
2023-04-03 22:10:01 +0300

    add login page

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app.routing.module';
 import LoginComponent from './auth/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     LoginComponent,
   ],
--- www/src/app/auth/login.component.ts
+++ www/src/app/auth/login.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [MatCardModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export default class LoginComponent {}
+export default class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  constructor(
+    private _iconRegistry: MatIconRegistry,
+    private _sanitizer: DomSanitizer
+  ) {
+    this._iconRegistry.addSvgIcon(
+      'padlock',
+      this._sanitizer.bypassSecurityTrustResourceUrl('/assets/img/padlock.svg')
+    );
+  }
+
+  login() {
+    this.loginForm.markAllAsTouched();
+
+    if (this.loginForm.valid) {
+      //
+    }
+  }
+}
